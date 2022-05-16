3891337caaf2783eedd9787063840dc0ed989d12
Sarah Laine <sarahlaine@Sarahs-MacBook-Pro.local>
2022-05-16 16:26:45 -0400

    install action text and active storage

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
